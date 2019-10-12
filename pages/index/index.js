d8d5144c8384dbf59630acc3dade875f9422b067
hotpoor <liwei.xia0223@gmail.com>
2019-10-12 15:14:46 +0800

    Merge孙浩钦的提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,9 +2,12 @@
 //获取应用实例
 const app = getApp()
 
+var cal_data=101300
+var press_data
+
 Page({
   data: {
-    dataArray:[0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81,0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81,0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81]
+    // dataArray:[0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81,0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81,0,78,79,80,81,78,79,80,81,78,79,80,81,79,80,81]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -12,36 +15,67 @@ Page({
       url: '../logs/logs'
     })
   },
-  canvasDrawData: function () {
+
+  clicktest: function(){
+    console.log("click")
+    cal_data=press_data+20
+    // this.setData({
+    //   style_name:"style2"
+    // });
+
+  },
+
+  canvasDrawData: function (arr) {
+    //console.log(arr)
     const ctx = wx.createCanvasContext('firstCanvas')
-    var maxh = 0
-    var minh = 0
-    var step = 3
-    let arr = this.data.dataArray
-    for (var j=0;j<arr.length;j++){
-      if (arr[j]>maxh){
-        maxh = arr[j]
-      }
-      if (arr[j]<minh){
-        minh = arr[j]
-      }
+    ctx.beginPath();
+    for(var i=0; i<5;i++){
+      ctx.moveTo(0,100*(i+1))
+      ctx.lineTo(340, 100*(i+1))
     }
-    var abs = maxh - minh
-    if(abs<160){
-      abs=160
+    ctx.strokeStyle="#E0E0E0"
+    ctx.stroke();
+
+    ctx.beginPath();
+    for(var i=0;i<7;i++){
+      ctx.fillText(100-i*20,5,100*(i));
+     //ctx.clearRect(0, 0, 300, 200)
     }
-    console.log("maxh:"+maxh+" minh:"+minh)
-    for (var i=0;i<arr.length;i++){
-      let x = 10+i*step
-      let y = 80+arr[i]*160/abs
-      if (i==0){
-        ctx.moveTo(x, y)
-      }
-      ctx.lineTo(x, y)
-      console.log("x:"+10+i*step+" y:"+arr[i])
+    ctx.moveTo(0, 500-arr[0]/40)
+    for(var i=0; i<arr.length;i++){
+      ctx.lineTo(i, 500-arr[i]/40)
     }
+    ctx.strokeStyle="#00eeee"
     ctx.stroke()
     ctx.draw()
+    // var maxh = 0
+    // var minh = 0
+    // var step = 3
+    // let arr = this.data.dataArray
+    // for (var j=0;j<arr.length;j++){
+    //   if (arr[j]>maxh){
+    //     maxh = arr[j]
+    //   }
+    //   if (arr[j]<minh){
+    //     minh = arr[j]
+    //   }
+    // }
+    // var abs = maxh - minh
+    // if(abs<160){
+    //   abs=160
+    // }
+    // console.log("maxh:"+maxh+" minh:"+minh)
+    // for (var i=0;i<arr.length;i++){
+    //   let x = 10+i*step
+    //   let y = 80+arr[i]*160/abs
+    //   if (i==0){
+    //     ctx.moveTo(x, y)
+    //   }
+    //   ctx.lineTo(x, y)
+    //   console.log("x:"+10+i*step+" y:"+arr[i])
+    // }
+    // ctx.stroke()
+    // ctx.draw()
     // console.log(canvasDrawData)
   },
 
@@ -65,10 +99,11 @@ Page({
         })
       },
       fail: function (res) {
-        wx.showModal({
-          title: '提示',
-          content: '请检查手机蓝牙是否打开',
-        })
+        console.log(res)
+        // wx.showModal({
+        //   title: '提示',
+        //   content: '请检查手机蓝牙是否打开',
+        // })
       },
     })
 
@@ -107,6 +142,7 @@ Page({
                               console.log('writeBLECharacteristicValue success', res.errMsg)
                             }
                           })
+                          var arr=[]
                           wx.notifyBLECharacteristicValueChange({
                             state: true, // 启用 notify 功能
                             deviceId:res.devices[0].deviceId,
@@ -116,20 +152,22 @@ Page({
                               console.log('notifyBLECharacteristicValueChange success', res.errMsg)
                               wx.onBLECharacteristicValueChange(function(jacob_data) {
                                 var n = String.fromCharCode.apply(null, new Uint8Array(jacob_data.value));
-                                var a = that.getCalibrationValue();
-                                var show_data = parseInt(n) - a
-                                console.log(show_data);
-
-                                var arr=that.data.dataArray
+                                press_data = parseInt(n)
+                                // var a = that.getCalibrationValue();
+                                var show_data = press_data - cal_data
+                                //console.log(show_data);
                                 arr.push(show_data)
-                                if(arr.length>200){
+                                if(arr.length>340){
                                   arr.shift();
                                 }
-                                console.log("arr_length"+arr.length)
-                                that.setData({
-                                  dataArray:arr
-                                });
-                                that.canvasDrawData();
+                                // console.log("arr_length"+arr.length)
+                                // that.setData({
+                                //   dataArray:arr
+                                // });
+                                // that.canvasDrawData();
+                                //console.log("arr_length"+arr.length)
+
+                                that.canvasDrawData(arr);
                               })
                             }
                           })
@@ -148,7 +186,8 @@ Page({
 
   onLoad: function () {
     this.startBLE();
-    this.canvasDrawData();
+    //var sysInfo=wx.getS
+    //this.canvasDrawData([265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209,265,267,284,221,209]);
   },
 
   getUserInfo: function(e) {
